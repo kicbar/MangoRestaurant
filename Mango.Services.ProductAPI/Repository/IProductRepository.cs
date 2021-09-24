80180e3cba512d91675c7489d7f65831b0bc16b4
kicbar <bartlomiejkicior@gmail.com>
2021-09-24 15:34:56 +0200

    add interfacce for product repository

--- Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Mango.Services.ProductAPI.Models.Dtos;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProduct(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
