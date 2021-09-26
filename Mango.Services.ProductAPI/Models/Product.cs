26b8f2a99910fd078ab31d9ce9f8d41b04adb5c0
kicbar <bartlomiejkicior@gmail.com>
2021-09-26 13:46:39 +0200

    seed data for products

--- Mango.Services.ProductAPI/Models/Product.cs
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -10,7 +10,7 @@ namespace Mango.Services.ProductAPI.Models
         [Required]
         public string Name { get; set; }
         [Range(1,1000)]
-        public double MyProperty { get; set; }
+        public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
