cbe86429fa086bdf50b9083edd613992b2e09d57
kicbar <bartlomiejkicior@gmail.com>
2021-09-26 13:21:54 +0200

    add GET method in controller

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,57 @@
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception exc)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string> { exc.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpGet()]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProduct(id);
+                _response.Result = productDto;
+            }
+            catch (Exception exc)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string> { exc.ToString() };
+            }
+            return _response;
+        }
+    }
+}
