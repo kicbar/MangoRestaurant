43b2af271dba2566ed5ebe722407131536da034a
kicbar <bartlomiejkicior@gmail.com>
2021-10-10 18:30:04 +0200

    Add Views for Product

---
+++ Mango.Services.ProductAPI/Migrations/20211010160418_UpdatePriceFld2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    public partial class UpdatePriceFld2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
