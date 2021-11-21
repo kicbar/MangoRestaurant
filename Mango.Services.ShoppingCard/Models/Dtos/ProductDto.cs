afe2de26bfdf6d68dc5aa91139876063c2f3fa7d
kicbar <bartlomiejkicior@gmail.com>
2021-11-21 22:04:42 +0100

    add models class

---
+++ Mango.Services.ShoppingCard/Models/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCardAPI.Models.Dtos
+{
+    public class ProductDto
+    {
+    }
+}
