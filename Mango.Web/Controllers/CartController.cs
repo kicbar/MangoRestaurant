1de8ce6c8166e9588c04dbface634ff64f38e487
kicbar <bartlomiejkicior@gmail.com>
2021-11-23 10:23:13 +0100

    add template view for shopping cart

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Controllers
+{
+    [ApiController]
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        private readonly IProductService _productService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _cartService = cartService;
+            _productService = productService;
+        }
+        public IActionResult CartIndex()
+        {
+            return View();
+        }
+    }
+}
