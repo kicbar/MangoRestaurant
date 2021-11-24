875996b6a6ab869e1c264c89c31ac1c4bd2b2c0e
kicbar <bartlomiejkicior@gmail.com>
2021-11-24 20:00:17 +0100

    add default setup for CouponAPI ms

---
+++ Mango.Services.CouponAPI/Mappings/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.CouponAPI.Mappings
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                //config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
